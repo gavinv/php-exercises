cd313392268d1862a3918597afd21e7a61ff2b28
gavinv <gavinvaught@gmail.com>
2016-07-05 14:26:32 -0500

    added fizzbuzz test

---
+++ fizzbuzz.php
@@ -0,0 +1,13 @@
+<?php
+for($i = 1; $i <= 100; $i++) {
+	if($i % 3 == 0 && $i % 5 == 0) {
+		echo 'FIZZBUZZ' . PHP_EOL;
+	} elseif($i % 5 == 0) {
+		echo 'BUZZ' . PHP_EOL;
+	} elseif($i % 3 == 0) {
+		echo 'FIZZ' . PHP_EOL;
+	} else {
+		echo $i . PHP_EOL;
+	}
+
+}
