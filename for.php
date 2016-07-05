e9d03f252a524e755eb0a24235628047ef06f813
gavinv <gavinvaught@gmail.com>
2016-07-05 12:06:39 -0500

    added if statement to default increment to 1 if there is no user input

--- for.php
+++ for.php
@@ -5,6 +5,9 @@ fwrite(STDOUT, 'Gimme an ending number: ');
 $endingNumber = trim(fgets(STDIN));
 fwrite(STDOUT, 'Choose increment: ');
 $increment = trim(fgets(STDIN));
+if(!is_numeric($increment)){
+	$increment = 1;
+}
 for($i = $startNumber; $i <= $endingNumber; $i += $increment) {
 	echo $i . PHP_EOL;
 }
