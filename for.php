461169c29dd2c9022c233697710cd4c3963c3ede
gavinv <gavinvaught@gmail.com>
2016-07-05 12:03:18 -0500

    refactored to take user input to choose increment (count by 1, 2, 10, ...)

--- for.php
+++ for.php
@@ -3,6 +3,8 @@ fwrite(STDOUT, 'Gimme a starting number: ');
 $startNumber = trim(fgets(STDIN));
 fwrite(STDOUT, 'Gimme an ending number: ');
 $endingNumber = trim(fgets(STDIN));
-for($i = $startNumber; $i <= $endingNumber; $i++) {
+fwrite(STDOUT, 'Choose increment: ');
+$increment = trim(fgets(STDIN));
+for($i = $startNumber; $i <= $endingNumber; $i += $increment) {
 	echo $i . PHP_EOL;
 }
