dbcc9b1a40e3e99fd52b3e1260ed0aa8b1037f06
gavinv <gavinvaught@gmail.com>
2016-07-05 16:00:37 -0500

    redid order to show all values in array

--- foreach.php
+++ foreach.php
@@ -2,11 +2,11 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-	if (is_scalar($thing)) {
-		echo $thing . PHP_EOL;
-	} elseif (is_array($thing)) {
+	if (is_array($thing)) {
 		foreach($thing as $other_things) {
 			echo $other_things . PHP_EOL;
 		}
+	} else {
+		echo $thing . PHP_EOL;
 	}
 }
