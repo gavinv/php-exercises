ef01a46c5f5dcecbdc720cad5dd1c9e472dea22a
gavinv <gavinvaught@gmail.com>
2016-07-06 11:55:32 -0500

    added test values for math functions

--- arithmetic.php
+++ arithmetic.php
@@ -1,18 +1,26 @@
 <?php
 function add($a, $b)
 {
-    return $a + $b;
+	return $a + $b;
 }
 function subtract($a, $b)
 {
-    // Add code here
+	return $a - $b;
 }
 function multiply($a, $b)
 {
-    // Add code here
+	return $a * $b;
 }
 function divide($a, $b)
 {
-    // Add code here
+	return $a / $b;
 }
-// Add code to test your functions here
+function modulus($a, $b)
+{
+	return $a % $b;
+}
+echo '5 + 4 = ' . add(5, 4) . PHP_EOL;
+echo '37 - 15 = ' . subtract(37, 15) . PHP_EOL;
+echo '12 * 95 = ' . multiply(12, 95) . PHP_EOL;
+echo '27 / 19 = ' . divide(27, 19) . PHP_EOL;
+echo '57 % 12 = ' . modulus(57, 12) . PHP_EOL;
