f386a43bb86a566fccfeac9bbbc66d731d7dec2f
gavinv <gavinvaught@gmail.com>
2016-07-07 15:22:26 -0500

    started working on internal functions. started adding switch cases for special types

--- arithmetic.php
+++ arithmetic.php
@@ -27,11 +27,12 @@ function multiply($a, $b)
 }
 function divide($a, $b)
 {
-	if(is_numeric($a) && is_numeric($b)) {
+	if(is_numeric($a) && is_numeric($b) && $b != 0) {
 		return $a / $b;
+	} elseif($b === 0) {
+		return "ERROR: Second argument cannot be 0 / cannot divide by 0" . PHP_EOL;
 	} else {
 		return "ERROR: Both arguments must be numbers \n";
-	}
 }
 function modulus($a, $b)
 {
@@ -41,10 +42,10 @@ function modulus($a, $b)
 		return "ERROR: Both arguments must be numbers \n";
 	}
 }
-echo 'a + 4 = ' . add(a, 4) . PHP_EOL;
+echo '5 + 4 = ' . add('a', 4) . PHP_EOL;
 echo '37 - 15 = ' . subtract(37, 15) . PHP_EOL;
 echo '12 * 95 = ' . multiply(12, 95) . PHP_EOL;
-echo '27 / 19 = ' . divide(27, 19) . PHP_EOL;
+echo '27 / 0 = ' . divide(27, 0) . PHP_EOL;
 echo '57 % 12 = ' . modulus(57, 12) . PHP_EOL;
 
 //make a square function
---
+++ internal_functions.php
@@ -0,0 +1,49 @@
+<?php
+// TODO: Create your inspect() function here
+function inspect($var) {
+	$vartype = gettype($var);
+	// return "The $vartype is $var";
+	switch($vartype) {
+		case 'NULL':
+			return "The value is NULL";
+			break;
+		case 'array':
+			if($var == []) {
+				return "The value is an empty array";
+			} else {
+				return "The value is an array";
+			}
+			break;
+		case 'boolean':
+			if($var == true) {
+				return "The value is TRUE";
+			} else {
+				return "The value is FALSE";
+			}
+			break;
+	}
+}
+// Do not mofify these variables!
+$string1 = "I'm a little teapot";
+$string2 = '';
+$array1 = [];
+$array2 = [1, 2, 3];
+$bool1 = true;
+$bool2 = false;
+$num1 = 0;
+$num2 = 0.0;
+$num3 = 12;
+$num4 = 14.4;
+$null = NULL;
+// TODO: After each echo statement, use inspect() to output the variable's type and its value
+echo 'Inspecting $num1: ' . inspect($num1) . PHP_EOL;
+echo 'Inspecting $num2: ' . inspect($num2) . PHP_EOL;
+echo 'Inspecting $num3: ' . inspect($num3) . PHP_EOL;
+echo 'Inspecting $num4: ' . inspect($num4) . PHP_EOL;
+echo 'Inspecting $null: ' . inspect($null) . PHP_EOL;
+echo 'Inspecting $bool1: ' . inspect($bool1) . PHP_EOL;
+echo 'Inspecting $bool2: ' . inspect($bool2) . PHP_EOL;
+echo 'Inspecting $string1: ' . inspect($string1) . PHP_EOL;
+echo 'Inspecting $string2: ' . inspect($string2) . PHP_EOL;
+echo 'Inspecting $array1: ' . inspect($array1) . PHP_EOL;
+echo 'Inspecting $array2: ' . inspect($array2) . PHP_EOL;
