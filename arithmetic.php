22e77da1e1e71dc541297ec7862ae49e74c7ed4b
gavinv <gavinvaught@gmail.com>
2016-07-07 11:53:57 -0500

    validated all arguments to only take numeric inputs

--- arithmetic.php
+++ arithmetic.php
@@ -3,26 +3,49 @@ $a = 2;
 $b = 10;
 function add($a, $b)
 {
-	return $a + $b;
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a + $b;
+	} else {
+		return "ERROR: Both arguments must be numbers \n";
+	}
 }
 function subtract($a, $b)
 {
-	return $a - $b;
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a - $b;
+	} else {
+		return "ERROR: Both arguments must be numbers \n";
+	}
 }
 function multiply($a, $b)
 {
-	return $a * $b;
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a * $b;
+	} else {
+		return "ERROR: Both arguments must be numbers \n";
+	}
 }
 function divide($a, $b)
 {
-	return $a / $b;
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a / $b;
+	} else {
+		return "ERROR: Both arguments must be numbers \n";
+	}
 }
 function modulus($a, $b)
 {
-	return $a % $b;
+	if(is_numeric($a) && is_numeric($b)) {
+		return $a % $b;
+	} else {
+		return "ERROR: Both arguments must be numbers \n";
+	}
 }
-echo '5 + 4 = ' . add(5, 4) . PHP_EOL;
+echo 'a + 4 = ' . add(a, 4) . PHP_EOL;
 echo '37 - 15 = ' . subtract(37, 15) . PHP_EOL;
 echo '12 * 95 = ' . multiply(12, 95) . PHP_EOL;
 echo '27 / 19 = ' . divide(27, 19) . PHP_EOL;
 echo '57 % 12 = ' . modulus(57, 12) . PHP_EOL;
+
+//make a square function
+//make a function called sum of squares using only above functions
