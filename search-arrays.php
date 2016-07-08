9501122c618155157032d5a291ece87f6979649b
gavinv <gavinvaught@gmail.com>
2016-07-08 14:23:41 -0500

    created function to return true or false if value passed is in selected array

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,13 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function findArray($array, $value) {
+	if(array_search($value, $array) !== false) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+echo findArray($names, 'Greg') . PHP_EOL;
