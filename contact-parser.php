5e9f7873813034b2d257668e23a8796c25ab23c4
gavinv <gavinvaught@gmail.com>
2016-07-11 15:13:06 -0500

    reworked function just a tad to make it more efficicent

--- contact-parser.php
+++ contact-parser.php
@@ -2,13 +2,12 @@
 function parse_contacts($filename)
 {
     $contacts = array();
-    $handle = fopen($filename, 'r');
-    $contents = fread($handle, filesize($filename));
+    $contents = trim(file_get_contents($filename));
     $contacts = explode("\n", $contents);
 	foreach($contacts as $contact) {
 		$splitContacts = explode("|", $contact);
-		$formatNumbers = substr($splitContacts[1], 0, 3) . '-' . substr($splitContacts[1], 3, 3) . '-' . substr($splitContacts[1], 6, 4);
-		$newArray[] = ['name' => $splitContacts[0], 'number' => $formatNumbers];
+		$formatNumber = substr($splitContacts[1], 0, 3) . '-' . substr($splitContacts[1], 3, 3) . '-' . substr($splitContacts[1], 6);
+		$newArray[] = ['name' => $splitContacts[0], 'number' => $formatNumber];
 	}
 	$contacts = $newArray;
     return $contacts;
