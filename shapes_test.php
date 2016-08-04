bca8c8e6b8babdcbdf6de58ce8b5173531d42d81
gavinv <gavinvaught@gmail.com>
2016-08-04 15:30:06 -0500

    created two tests for the area and perimeter of one square

--- shapes_test.php
+++ shapes_test.php
@@ -1,5 +1,5 @@
 <?php
-require_once 'Rectangle.php';
+require_once 'Square.php';
 
 $rectangleOne = new Rectangle(3, 4);
 $rectangleTwo = new Rectangle(10, 17);
@@ -8,3 +8,7 @@ $rectangleThree = new Rectangle(6, 16);
 echo $rectangleOne->area() . PHP_EOL;
 echo $rectangleTwo->area() . PHP_EOL;
 echo $rectangleThree->area() . PHP_EOL;
+
+$squareOne = new Square(5, 5);
+echo $squareOne->perimeter() . PHP_EOL;
+echo $squareOne->area() . PHP_EOL;
