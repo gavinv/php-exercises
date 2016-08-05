f218dede141a2555a3f8477f82d6a6dfb7b76de6
gavinv <gavinvaught@gmail.com>
2016-08-05 15:36:00 -0500

    created Model class with properties key and value that takes them and creates an array

---
+++ Model.php
@@ -0,0 +1,22 @@
+<?php
+
+class Model {
+
+	private $attributes = [];
+	public function __set($key, $value)
+    {
+        $this->attributes[$key] = $value;
+    }
+    public function __get($key)
+    {
+        if (array_key_exists($key, $this->attributes)) {
+            return $this->attributes[$key];
+        }
+
+        return null;
+    }
+}
+
+$model = new Model();
+$model->key = 'gavin';
+print_r($model);
